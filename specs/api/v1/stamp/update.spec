a22a6e41469bccc4cc02e4b36e58625c717ff832
 Shin Yamaga <s_yamaga@codmon.co.jp>
2023-05-05 15:04:14 +0900

    postリクエストのテスト実装完了

--- specs/api/v1/stamp/update.spec
+++ specs/api/v1/stamp/update.spec
@@ -1,4 +1,4 @@
 # src/api/v1/stamp/update.php
 ## Happy path
-* Send POST request to "http://localhost:8000/src/api/v1/stamp/update.php?company_id=1884310&employee_id=1164735" with "requests/api/v1/stamp/update.json"
-Responced status equals to "200"
+* Send POST request to "http://localhost:8000/src/api/v1/stamp/update.php" with "requests/api/v1/stamp/update.json"
+* Responced status equals to "200"
--- tests/AttendanceAppE2e.ts
+++ tests/AttendanceAppE2e.ts
@@ -3,28 +3,29 @@ import { strictEqual } from 'assert';
 import { checkBox, click, closeBrowser, evaluate, goto, into, link, openBrowser, press, text, textBox, toLeftOf, write } from 'taiko';
 import assert = require("assert");
 import axios, { AxiosResponse } from "axios";
-import {DataStoreFactory, DataStore} from 'gauge-ts';
+import { DataStoreFactory, DataStore } from 'gauge-ts';
 import * as fs from 'fs'
 
 export default class AttendanceAppE2e {
 
     @Step("Send GET request to <url>")
     public async handleGetRequest(uri: string): Promise<any> {
-        const responce = await this.get(uri)
+        const responce = await this.sendGetRequest(uri)
         DataStoreFactory.getScenarioDataStore().remove("status_code");
         DataStoreFactory.getScenarioDataStore().remove("data")
         DataStoreFactory.getScenarioDataStore().put("status_code", responce.status);
         DataStoreFactory.getScenarioDataStore().put("data", responce.data);
     }
 
-    @Step("Send POST request to <url> with <relativePath>")
+    @Step("Send POST request to <uri> with <relativePath>")
     public async handlePostRequest(uri: string, relativePath: string): Promise<any> {
-        const responce = await this.post(uri, relativePath)
-        // const responce = await this.post(uri, relativePath)
-        // DataStoreFactory.getScenarioDataStore().remove("status_code");
-        // DataStoreFactory.getScenarioDataStore().remove("data")
-        // DataStoreFactory.getScenarioDataStore().put("status_code", responce.status);
-        // DataStoreFactory.getScenarioDataStore().put("data", responce.data);
+        const fullPath = this.getPathToGauge() + relativePath;
+        const jsonData = JSON.parse(fs.readFileSync(fullPath).toString());
+        const responce = await this.sendPostRequest(uri, jsonData)
+        DataStoreFactory.getScenarioDataStore().remove("status_code");
+        DataStoreFactory.getScenarioDataStore().remove("data")
+        DataStoreFactory.getScenarioDataStore().put("status_code", responce.status);
+        DataStoreFactory.getScenarioDataStore().put("data", responce.data);
     }
 
     @Step("Responced status equals to <statusCode>")
@@ -42,29 +43,30 @@ export default class AttendanceAppE2e {
         );
     }
 
-    private async get(uri: string): Promise<any> {
-        const response = await axios.get<[]>(uri);
-        return response;
+    private async sendGetRequest(uri: string): Promise<any> {
+        try {
+            const response = await axios.get<[]>(uri);
+            return response
+        } catch (error) {
+            return error
+        }
     }
 
-    private async post(uri: string, relativePath: string): Promise<any> {
-        // const fullPath = this.getPathToGauge() + relativePath;
-        // const PostBodyJson = fs.readFileSync(fullPath).toString();
-        const data = {
-            company_id : '1884310',  
-            employee_id : '1164735'
-        };
-        const response = await axios.post(uri, data);
-        return response;
-    }
-
-    private getPathToGauge(): string{
+    private getPathToGauge(): string {
         require('dotenv').config();
         return process.env.PATH_TO_PROJECT;
     }
-}
 
-interface IPostRequest {
-    companyId: number
-    employeeId: number
+    private async sendPostRequest(url: string, requestBody: any) {
+        try {
+            const response = await axios.post(url, requestBody, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                }
+            });
+            return response
+        } catch (error) {
+            return error
+        }
+    }
 }
